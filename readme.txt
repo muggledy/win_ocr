3412ef8071df668d8ebcbbce09af4dfd2803c8d0
muggledy <zgjsycfndy2015@163.com>
2023-12-19 20:19:34 +0800

    update snip_ocr.bat

--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-1. install python from https://www.python.org/downloads/ and pip install baidu-aip
-2. set your APP_ID, API_KEY and SECRET_KEY in baidu_ocr.py
-3. install AutoHotKey from https://www.autohotkey.com/ and double click to execute myhotkey.ahk
-4. now you can take screenshots interactively via shortcut keys Ctrl+Alt+X and trigger recognizing text by closing SnippingTool, the recognition results are written to the clipboard
+1. Install python from https://www.python.org/downloads/ and pip install baidu-aip
+2. Set your APP_ID, API_KEY and SECRET_KEY in baidu_ocr.py
+3. Install AutoHotKey from https://www.autohotkey.com/ and double click myhotkey.ahk
+4. Now you can take screenshots interactively in 15 seconds via shortcut keys Ctrl+Alt+X, the recognition results will be written into the clipboard. If there is no screenshot operation within 15 seconds, the text recognition program will terminate. You need to press the shortcut keys again to take a new screenshot for text recognition
--- snip_ocr.bat
+++ snip_ocr.bat
@@ -11,14 +11,26 @@ set "py_script_path=%current_path%\baidu_ocr.py"
 set "tmp_img_path=%current_path%\tmp.png"
 set "tmp_img_output_path=%current_path%\tmp.png.output"
 set "nircmd_path=%current_path%\nircmd.exe"
+set "lock_file_path=%current_path%\lockfile"
+set seconds=4 2 1 1 1 1 1 1 1 1 1
+set state=success
+set /a seconds_total=0
 
+echo baidu ocr...
 if not exist %nircmd_path% (
-    echo Error: nircmd.exe not exist
+    echo Error: nircmd.exe not exist!
     exit /b 1
 )
 if not exist %py_script_path% (
-    echo Error: baidu_ocr.py not exist
+    echo Error: baidu_ocr.py not exist!
     exit /b 1
+)
+
+if exist %lock_file_path% (
+    echo Error: locked, cannot re-exec snip_ocr.bat!
+    exit /b 1
+) else (
+    echo. > %lock_file_path%
 )
 
 if exist "%tmp_img_path%" (
@@ -29,21 +41,35 @@ if exist "%tmp_img_output_path%" (
 )
 %nircmd_path% clipboard clear
 
-for /f "tokens=2 delims= " %%a in ('tasklist /fi "imagename eq SnippingTool.exe" ^| find "SnippingTool.exe"') do set "PID=%%a"
-if not "%PID%"=="" (
-    echo Info: kill existed SnippingTool process %PID%
-    taskkill /F /PID %PID%
+start "" /b SnippingTool.exe /clip
+
+for %%N in (%seconds%) do (
+    set /a seconds_total+=%%N
+    timeout /t %%N /nobreak
+    %nircmd_path% clipboard saveimage %tmp_img_path%
+    if exist "%tmp_img_path%" (
+        python %py_script_path% %tmp_img_path%
+        goto :exitloop
+    )
 )
 
-SnippingTool.exe /clip
-%nircmd_path% clipboard saveimage %tmp_img_path%
-
-if exist "%tmp_img_path%" (
-    python %py_script_path% %tmp_img_path%
-) else (
-    echo Error: snip failed
+if not exist "%tmp_img_path%" (
+    echo Error: snip failed in %seconds_total% seconds!
+    set state=fail
+    goto :unlock
 )
+:exitloop
 if exist "%tmp_img_output_path%" (
     %nircmd_path% clipboard clear
     %nircmd_path% clipboard readfile %tmp_img_output_path%
+) else (
+    echo Error: recognize failed!
+    set state=fail
+)
+:unlock
+if exist %lock_file_path% (
+    del %lock_file_path%
+)
+if "%state%"=="fail" (
+    exit /b 1
 )
